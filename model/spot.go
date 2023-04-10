d0c4cf33e06ded47fb7025489671f87de9efae48
tatsuro1997 <minagawa@dptr.jp>
2023-04-11 08:17:23 +0900

    Spotモデルの作成

---
+++ model/spot.go
@@ -0,0 +1,20 @@
+package model
+
+import "time"
+
+type Spot struct {
+	ID          int64     `json:"id"`
+	Name        string    `json:"name"`
+	Image       string    `json:"image"`
+	Type        string    `json:"type"`
+	Address     string    `json:"address"`
+	HpURL       string    `json:"hp_url"`
+	OpenTime    string    `json:"open_time"`
+	OffDay      string    `json:"off_day"`
+	Parking     bool      `json:"parking"`
+	Description string    `json:"string"`
+	Lat         float32   `json:"lat"`
+	Lng         float32   `json:"lng"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
