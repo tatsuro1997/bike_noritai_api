dff079a4d4ad43ff15edc415344e8ab3e3c7d902
tatsuro1997 <minagawa@dptr.jp>
2023-05-05 07:03:36 +0900

    add comment

--- model/comment.go
+++ model/comment.go
@@ -6,7 +6,7 @@ type Comment struct {
 	ID        int64     `json:"id"`
 	UserID    int64     `json:"user_id"`
 	RecordID  int64     `json:"record_id"`
-	UserName  string    `json:"user_name"`
+	UserName  string    `json:"user_name"` // TODO: remove consideration
 	Text      string    `json:"text"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
