95e14e4b68fc36fdd26addc4959dc4a621366517
tatsuro1997 <minagawa@dptr.jp>
2023-05-06 07:22:42 +0900

    add TestCreateBookmark

--- test/bookmark_test.go
+++ test/bookmark_test.go
@@ -2,16 +2,16 @@ package test
 
 import (
 	// "bytes"
-	// "encoding/json"
+	"encoding/json"
 	// "errors"
 	// "gorm.io/gorm"
 	"net/http"
 	"net/http/httptest"
-	// "strconv"
+	"strconv"
 	"strings"
 	"testing"
 
-	// . "bike_noritai_api/model"
+	. "bike_noritai_api/model"
 	// . "bike_noritai_api/repository"
 	. "bike_noritai_api/router"
 )
@@ -38,74 +38,35 @@ func TestGetBookmarks(t *testing.T) {
 	}
 }
 
-// func TestGetRecordComments(t *testing.T) {
-// 	router := NewRouter()
-// 	req := httptest.NewRequest(http.MethodGet, "/api/records/2/comments", nil)
-// 	res := httptest.NewRecorder()
-// 	router.ServeHTTP(res, req)
+func TestCreateBookmark(t *testing.T) {
+	router := NewRouter()
 
-// 	if res.Code != http.StatusOK {
-// 		t.Errorf("unexpected status code: got %v, want %v", res.Code, http.StatusOK)
-// 	}
+	var userID int64 = 3
+	var spotID int64 = 3
 
-// 	expectedBody := `"id":3,"user_id":2,"record_id":2,"user_name":"tester1","text":"CCCCCCCCCCCCCCCCC"`
+	req := httptest.NewRequest(http.MethodPost, "/api/users/"+strconv.Itoa(int(userID))+"/spots/"+strconv.Itoa(int(spotID))+"/bookmarks", nil)
+	req.Header.Set("Content-Type", "application/json")
+	res := httptest.NewRecorder()
+	router.ServeHTTP(res, req)
 
-// 	expectedBody2 := `"id":4,"user_id":2,"record_id":2,"user_name":"tester1","text":"DDDDDDDDDDDDDDDDDD"`
+	if res.Code != http.StatusCreated {
+		t.Errorf("expected status code %v but got %v", http.StatusCreated, res.Code)
+	}
 
-// 	if !strings.Contains(res.Body.String(), expectedBody) {
-// 		t.Errorf("unexpected response body: got %v, want %v", res.Body.String(), expectedBody)
-// 	}
-
-// 	if !strings.Contains(res.Body.String(), expectedBody2) {
-// 		t.Errorf("unexpected response body: got %v, want %v", res.Body.String(), expectedBody2)
-// 	}
-// }
-
-// func TestCreateComment(t *testing.T) {
-// 	router := NewRouter()
-
-// 	comment := Comment{
-// 		UserName: "Tester",
-// 		Text:     "EEEEEEEEEEEEEEEEEEEE",
-// 	}
-
-// 	reqBody, err := json.Marshal(comment)
-// 	if err != nil {
-// 		t.Fatalf("failed to marshal request body: %v", err)
-// 	}
-
-// 	var userID int64 = 1
-// 	var recordID int64 = 1
-
-// 	req := httptest.NewRequest(http.MethodPost, "/api/users/"+strconv.Itoa(int(userID))+"/records/"+strconv.Itoa(int(recordID))+"/comments", bytes.NewBuffer(reqBody))
-// 	req.Header.Set("Content-Type", "application/json")
-// 	res := httptest.NewRecorder()
-// 	router.ServeHTTP(res, req)
-
-// 	if res.Code != http.StatusCreated {
-// 		t.Errorf("expected status code %v but got %v", http.StatusCreated, res.Code)
-// 	}
-
-// 	var resBody Comment
-// 	if err := json.Unmarshal(res.Body.Bytes(), &resBody); err != nil {
-// 		t.Fatalf("failed to unmarshal response body: %v", err)
-// 	}
-// 	if resBody.ID == 0 {
-// 		t.Errorf("expected spot ID to be non-zero but got %v", resBody.ID)
-// 	}
-// 	if resBody.UserID != userID {
-// 		t.Errorf("expected comment user id to be %v but got %v", userID, resBody.UserID)
-// 	}
-// 	if resBody.UserID != recordID {
-// 		t.Errorf("expected comment record id to be %v but got %v", recordID, resBody.UserID)
-// 	}
-// 	if resBody.UserName != comment.UserName {
-// 		t.Errorf("expected comment user name to be %v but got %v", comment.UserName, resBody.UserName)
-// 	}
-// 	if resBody.Text != comment.Text {
-// 		t.Errorf("expected comment text to be %v but got %v", comment.Text, resBody.Text)
-// 	}
-// }
+	var resBody Bookmark
+	if err := json.Unmarshal(res.Body.Bytes(), &resBody); err != nil {
+		t.Fatalf("failed to unmarshal response body: %v", err)
+	}
+	if resBody.ID == 0 {
+		t.Errorf("expected spot ID to be non-zero but got %v", resBody.ID)
+	}
+	if resBody.UserID != userID {
+		t.Errorf("expected comment user id to be %v but got %v", userID, resBody.UserID)
+	}
+	if resBody.SpotID != spotID {
+		t.Errorf("expected comment record id to be %v but got %v", spotID, resBody.UserID)
+	}
+}
 
 // func TestUpdateComment(t *testing.T) {
 // 	comment := Comment{
